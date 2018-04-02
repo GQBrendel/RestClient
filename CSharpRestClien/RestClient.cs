c7c6e916ab34775f6029dd4e502f60495b87f7e9
GuilhermeQB <guilherme.brendel@owlbytes.com>
2018-04-02 14:52:37 -0300

    Error Handling when making a request

--- CSharpRestClien/RestClient.cs
+++ CSharpRestClien/RestClient.cs
@@ -27,43 +27,41 @@ namespace CSharpRestClien
         public string MakeRequest()
         {
             string strResponseValue = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+            request.Method = httpMethod.ToString();
+
+            HttpWebResponse response = null;
             try
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
-                request.Method = httpMethod.ToString();
-
-                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode != HttpStatusCode.OK)
                 {
-                    if (response.StatusCode != HttpStatusCode.OK)
-                    {
-                        throw new ApplicationException("Error Code: " + response.StatusCode);
-                    }
-                    //Process the Response Stream... (could be JSON, XML or HTML, etc...)
+                    throw new ApplicationException("Error Code: " + response.StatusCode);
+                }
+                //Process the Response Stream... (could be JSON, XML or HTML, etc...)
 
-                    using (Stream responseStream = response.GetResponseStream())
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
                     {
-                        if (responseStream != null)
+                        using (StreamReader reader = new StreamReader(responseStream))
                         {
-                            using (StreamReader reader = new StreamReader(responseStream))
-                            {
-                                strResponseValue = reader.ReadToEnd();
-                            }//End of using StreamReader
-                        }
-                    }//End orf using responseStream
-                }//End of Using Response
-
+                            strResponseValue = reader.ReadToEnd();
+                        }//End of using StreamReader
+                    }
+                }//End orf using responseStream
             }
-            catch (WebException webEx)
+            catch (Exception ex)
             {
-                System.Diagnostics.Debug.Write("Exception Message: " + webEx.Message + Environment.NewLine);
-
-                // Console.WriteLine("Exception Message: " + ex.Message);
+                strResponseValue = "{\"errorMessages\":[\"" + ex.Message.ToString() + "\"],\"errors\":{}}";
             }
-            catch (FormatException formatEx)
+            finally
             {
-                System.Diagnostics.Debug.Write("Exception Message: " + formatEx.Message + Environment.NewLine);
+                if(response !=  null)
+                {
+                    ((IDisposable)response).Dispose();
+                }
             }
-
             return strResponseValue;
         }
     }
